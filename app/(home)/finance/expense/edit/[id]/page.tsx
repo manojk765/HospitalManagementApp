a2536d62b7d0deeb48d9b653e265816a9347cbc8
manojk765 <manojkoneti05@gmail.com>
2025-02-23 20:33:59 +0530

    removed await

--- app/(home)/finance/expense/edit/[id]/page.tsx
+++ app/(home)/finance/expense/edit/[id]/page.tsx
@@ -1,16 +1,14 @@
 import EditExpenseForm from "./EditExpenseForm";
 
-// Update the type to reflect that params is a Promise
 interface PageProps {
-  params: Promise<{
+  params: {
     id: string;
-  }>;
+  };
   searchParams: { [key: string]: string | string[] | undefined };
 }
 
 export default async function ExpensePage({ params }: PageProps) {
-  // Destructure params after awaiting
-  const { id } = await params;
+  const { id } = params;
   
   return <EditExpenseForm id={id} />;
 }
